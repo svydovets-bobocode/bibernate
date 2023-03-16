d296798bde25ac6352b1fb0bdeffe3fc9b9651a0
Stanislav <33624178+rnyPlanet@users.noreply.github.com>
2023-03-16 19:00:30 +0200

    BN-12 Create @Column annotation and integrate process logic (#6)
    * BN-12 Create @Column annotation and integrate process logic

--- src/test/java/com/bobocode/svydovets/bibernate/util/EntityUtilsTest.java
+++ src/test/java/com/bobocode/svydovets/bibernate/util/EntityUtilsTest.java
@@ -3,12 +3,21 @@ package com.bobocode.svydovets.bibernate.util;
 import static com.bobocode.svydovets.bibernate.util.Constants.CLASS_HAS_NO_ARG_CONSTRUCTOR;
 import static com.bobocode.svydovets.bibernate.util.Constants.CLASS_HAS_NO_ENTITY_ANNOTATION;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.bobocode.svydovets.bibernate.exception.EntityValidationException;
 import com.bobocode.svydovets.bibernate.testdata.entity.EntityWithoutNonArgConstructor;
 import com.bobocode.svydovets.bibernate.testdata.entity.NonEntityClass;
+import com.bobocode.svydovets.bibernate.testdata.entity.Person;
+import com.bobocode.svydovets.bibernate.testdata.entity.User;
+import java.lang.reflect.Field;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class EntityUtilsTest {
     @Test
@@ -27,5 +36,56 @@ class EntityUtilsTest {
                 .withMessage(
                         String.format(
                                 CLASS_HAS_NO_ARG_CONSTRUCTOR, EntityWithoutNonArgConstructor.class.getName()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("entityWithFieldNamesSource")
+    @DisplayName("Resolve column name")
+    void resolveColumnName(Class<?> entityClass, String fieldName, String expected)
+            throws NoSuchFieldException {
+        var result = EntityUtils.resolveColumnName(entityClass.getDeclaredField(fieldName));
+        assertEquals(expected, result);
+    }
+
+    private static Stream<Arguments> entityWithFieldNamesSource() {
+        return Stream.of(
+                Arguments.of(Person.class, "firstName", "firstName"),
+                Arguments.of(Person.class, "lastName", "last_name"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("entityWithInsertableFieldSource")
+    @DisplayName("Get insertable fields")
+    void getInsertableFields(Class<?> entityClass, Field[] expected) {
+        var result = EntityUtils.getInsertableFields(entityClass);
+        assertArrayEquals(expected, result);
+    }
+
+    private static Stream<Arguments> entityWithInsertableFieldSource() throws NoSuchFieldException {
+        return Stream.of(
+                Arguments.of(Person.class, new Field[] {Person.class.getDeclaredField("firstName")}),
+                Arguments.of(
+                        User.class,
+                        new Field[] {
+                            User.class.getDeclaredField("name"), User.class.getDeclaredField("phone")
+                        }));
+    }
+
+    @ParameterizedTest
+    @MethodSource("entityWithUpdatableFieldSource")
+    @DisplayName("Get updatable fields")
+    void getUpdatableFields(Class<?> entityClass, Field[] expected) {
+        var result = EntityUtils.getUpdatableFields(entityClass);
+        assertArrayEquals(expected, result);
+    }
+
+    private static Stream<Arguments> entityWithUpdatableFieldSource() throws NoSuchFieldException {
+        return Stream.of(
+                Arguments.of(Person.class, new Field[] {Person.class.getDeclaredField("lastName")}),
+                Arguments.of(
+                        User.class,
+                        new Field[] {
+                            User.class.getDeclaredField("name"), User.class.getDeclaredField("phone")
+                        }));
     }
 }
