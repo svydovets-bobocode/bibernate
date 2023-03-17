b0fdab61b8cc36f153f24992e278cc8c85847e65
Roman Kuznetsov <roman.kuznetsov@playtech.com>
2023-03-17 11:14:19 +0200

    Refactored todo comments

--- src/main/java/com/bobocode/svydovets/bibernate/config/BibernateConfiguration.java
+++ src/main/java/com/bobocode/svydovets/bibernate/config/BibernateConfiguration.java
@@ -7,31 +7,33 @@ import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-//todo: add JavaDoc and README guide. This is the starting point for our consumers to use the framework
+// todo: add JavaDoc and README guide.
+// todo: This is the starting point for our consumers to use the framework
 public class BibernateConfiguration {
     private ConfigurationSource source;
     private final DataSource dataSource;
     private final SqlQueryBuilder sqlQueryBuilder;
 
-    //todo: should it be public? Consumer should use only "configure" and "buildSessionFactory" methods
+    // todo: should it be public?
+    // todo: Consumer should use only "configure" and "buildSessionFactory" methods
     public BibernateConfiguration(DataSource dataSource, SqlQueryBuilder sqlQueryBuilder) {
         this.dataSource = dataSource;
         this.sqlQueryBuilder = sqlQueryBuilder;
     }
 
-    //todo: add JavaDoc and README guide how to use default configuration
+    // todo: add JavaDoc and README guide how to use default configuration
     public BibernateConfiguration configure() {
         return configure(new PropertyFileConfiguration("bibernate.properties"));
     }
 
-    //todo: add JavaDoc and README guide how to configure with different configuration sources
+    // todo: add JavaDoc and README guide how to configure with different configuration sources
     public BibernateConfiguration configure(ConfigurationSource source) {
         log.info("Configuring with source: {}", source);
         this.source = source;
         return this;
     }
 
-    //todo: add JavaDoc and README guide how to obtain the SessionFactory.
+    // todo: add JavaDoc and README guide how to obtain the SessionFactory.
     public SessionFactory buildSessionFactory() {
         if (source == null) {
             throw new IllegalStateException(
@@ -41,7 +43,8 @@ public class BibernateConfiguration {
                 "Building SessionFactory with DataSource: {} and SqlQueryBuilder: {}",
                 dataSource,
                 sqlQueryBuilder);
-        //todo: Should we return new every time? Maybe it should be the single object of the SessionFactory per app
+        // todo: Should we return new every time?
+        // todo: Maybe it should be the single object of the SessionFactory per app
         return new SessionFactoryImpl(dataSource, sqlQueryBuilder);
     }
 }
