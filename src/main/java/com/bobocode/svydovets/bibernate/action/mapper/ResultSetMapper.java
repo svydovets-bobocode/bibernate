10e49c92d9953970f4b1b52eb028ceca54f96769
Roman Kuznetsov <roman.kuznetsov@playtech.com>
2023-03-19 20:18:52 +0200

    BN-29 Refactor ResultSetMapper, add error handling, annotation and data types processing

--- src/main/java/com/bobocode/svydovets/bibernate/action/mapper/ResultSetMapper.java
+++ src/main/java/com/bobocode/svydovets/bibernate/action/mapper/ResultSetMapper.java
@@ -1,23 +1,72 @@
 package com.bobocode.svydovets.bibernate.action.mapper;
 
+import static com.bobocode.svydovets.bibernate.util.Constants.ERROR_MAPPING_RESULT_SET_TO_OBJECT;
+
+import com.bobocode.svydovets.bibernate.annotation.Column;
+import com.bobocode.svydovets.bibernate.exception.BibernateException;
 import java.lang.reflect.Field;
+import java.sql.Date;
 import java.sql.ResultSet;
-import lombok.SneakyThrows;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.apache.commons.lang3.StringUtils;
 
 public class ResultSetMapper {
 
-    @SneakyThrows // Todo: replace with custom exceptions
     public static <T> T mapToObject(Class<T> type, ResultSet resultSet) {
-        T instance = type.getDeclaredConstructor().newInstance();
+        try {
+            return mapResultSetToObject(type, resultSet);
+        } catch (Exception e) {
+            throw new BibernateException(ERROR_MAPPING_RESULT_SET_TO_OBJECT.formatted(type.getName()), e);
+        }
+    }
+
+    public static <T> T mapResultSetToObject(Class<T> type, ResultSet resultSet) throws Exception {
+        T instance = createEmptyInstance(type);
         while (resultSet.next()) {
             Field[] fields = type.getDeclaredFields();
             for (Field field : fields) {
-                // TODO: Handle annotations and field types
-                Object fieldValue = resultSet.getObject(field.getName(), field.getType());
                 field.setAccessible(true);
-                field.set(instance, fieldValue);
+                String columnName = resolveColumnName(field);
+                Object compatibleObject =
+                        convertObjectToCompatibleType(resultSet.getObject(columnName), field);
+                field.set(instance, compatibleObject);
             }
         }
         return instance;
+    }
+
+    private static <T> T createEmptyInstance(Class<T> type) {
+        try {
+            return type.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot create an instance using the default constructor", e);
+        }
+    }
+
+    private static String resolveColumnName(Field field) {
+        if (field.isAnnotationPresent(Column.class)) {
+            String explicitName = field.getAnnotation(Column.class).name();
+            if (StringUtils.isNotBlank(explicitName)) {
+                return explicitName;
+            }
+        }
+        return field.getName();
+    }
+
+    // TODO: handle more specific data types
+    private static Object convertObjectToCompatibleType(Object object, Field field) {
+        Class<?> fieldType = field.getType();
+
+        if (fieldType.isAssignableFrom(LocalDate.class)) {
+            return ((Date) object).toLocalDate();
+        }
+
+        if (fieldType.isAssignableFrom(LocalDateTime.class)) {
+            return ((Timestamp) object).toLocalDateTime();
+        }
+
+        return object;
     }
 }
--- src/main/java/com/bobocode/svydovets/bibernate/util/Constants.java
+++ src/main/java/com/bobocode/svydovets/bibernate/util/Constants.java
@@ -9,4 +9,6 @@ public final class Constants {
             "Entity '%s' has no 'no-arg constructor' (every '@Entity' class must declare 'no-arg constructor')";
     public static final String CAN_NOT_CREATE_A_SNAPSHOT_OF_ENTITY =
             "Can not create a snapshot object for entity";
+    public static final String ERROR_MAPPING_RESULT_SET_TO_OBJECT =
+            "Error while mapping result set to object of type '%s'";
 }
