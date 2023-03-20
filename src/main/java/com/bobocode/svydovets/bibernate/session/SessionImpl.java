c3a906c77db789f198b7a934f8754cbf90846f7f
Valerii Fedorenko <valerii.fedorenko.ua@gmail.com>
2023-03-20 19:47:27 +0200

    BR-6: Implement Session#close() (#13)
    * [BR-6] Implement Session close method
    
    * BR-6 Resolved merge conflicts
    
    ---------
    
    Co-authored-by: Roman Kuznetsov <roman.kuznetsov@playtech.com>

--- src/main/java/com/bobocode/svydovets/bibernate/session/SessionImpl.java
+++ src/main/java/com/bobocode/svydovets/bibernate/session/SessionImpl.java
@@ -2,6 +2,7 @@ package com.bobocode.svydovets.bibernate.session;
 
 import com.bobocode.svydovets.bibernate.action.SelectAction;
 import com.bobocode.svydovets.bibernate.action.key.EntityKey;
+import com.bobocode.svydovets.bibernate.constant.ErrorMessage;
 import com.bobocode.svydovets.bibernate.exception.BibernateException;
 import com.bobocode.svydovets.bibernate.transaction.Transaction;
 import com.bobocode.svydovets.bibernate.transaction.TransactionImpl;
@@ -10,91 +11,108 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import lombok.RequiredArgsConstructor;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-@RequiredArgsConstructor
 public class SessionImpl implements Session {
-    private SelectAction selectAction;
+    private final SelectAction selectAction;
     private final Connection connection;
-    private Transaction transaction;
-    private boolean isOpen;
-    private final Map<EntityKey<?>, Object> persistenceContext = new ConcurrentHashMap<>();
+    private final Transaction transaction;
+
+    private final Map<EntityKey<?>, Object> entititiesCacheMap = new ConcurrentHashMap<>();
     private final Map<EntityKey<?>, Object[]> entitiesSnapshotMap = new ConcurrentHashMap<>();
+
+    private final AtomicBoolean isOpen = new AtomicBoolean(true);
 
     public SessionImpl(SelectAction selectAction, Connection connection) {
         this.selectAction = selectAction;
         this.connection = connection;
         this.transaction = new TransactionImpl(connection);
-        this.isOpen = true;
     }
 
     @Override
     public <T> T find(Class<T> type, Object id) {
+        verifySessionIsOpened();
         EntityKey<T> entityKey = new EntityKey<>(type, id);
-        return type.cast(persistenceContext.computeIfAbsent(entityKey, selectAction::execute));
+        return type.cast(entititiesCacheMap.computeIfAbsent(entityKey, selectAction::execute));
     }
 
     @Override
     public <T> T save(T entity) {
+        verifySessionIsOpened();
         return null;
     }
 
     @Override
-    public <T> void delete(T id) {}
+    public <T> void delete(T id) {
+        verifySessionIsOpened();
+    }
 
     @Override
     public <T> List<T> findAll(Class<T> type) {
+        verifySessionIsOpened();
         return null;
     }
 
     @Override
     public <T> List<T> findAll(Class<T> type, Map<String, Object> properties) {
+        verifySessionIsOpened();
         return null;
     }
 
     @Override
     public void close() {
         try {
-            isOpen = false;
-            connection.close();
+            flush();
+            entititiesCacheMap.clear();
+            entitiesSnapshotMap.clear();
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+            isOpen.set(false);
         } catch (SQLException e) {
-            throw new BibernateException("Unable to close session", e);
+            throw new BibernateException("Something went wrong during closing the session", e);
         }
     }
 
     @Override
     public <T> T merge(T entity) {
+        verifySessionIsOpened();
         return null;
     }
 
     @Override
-    public void detach(Object entity) {}
+    public void detach(Object entity) {
+        verifySessionIsOpened();
+    }
 
     @Override
-    public void flush() {}
+    public void flush() {
+        verifySessionIsOpened();
+    }
 
     @Override
     public void begin() {
-        checkIsOpen();
+        verifySessionIsOpened();
         transaction.begin();
     }
 
     @Override
     public void commit() {
-        checkIsOpen();
-        // todo: flush()???
+        verifySessionIsOpened();
+        flush();
         transaction.commit();
     }
 
     @Override
     public void rollback() {
-        checkIsOpen();
+        // TODO: think about do we need to clear the persistence context maps
+        verifySessionIsOpened();
         transaction.rollback();
     }
 
-    private void checkIsOpen() {
-        if (!isOpen) {
-            throw new BibernateException("Session is already closed");
+    private void verifySessionIsOpened() {
+        if (!isOpen.get()) {
+            throw new BibernateException(ErrorMessage.SESSION_IS_CLOSED);
         }
     }
 }
