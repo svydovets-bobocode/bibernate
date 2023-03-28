5c16e0b98d66ec2c58aa6498f5c54ade362e65b5
Mykola Klushyn <mklushyn@atlassian.com>
2023-03-28 15:15:24 +0200

    delete tests

--- src/test/java/com/bobocode/svydovets/bibernate/testdata/factory/TestPersonFactory.java
+++ src/test/java/com/bobocode/svydovets/bibernate/testdata/factory/TestPersonFactory.java
@@ -22,13 +22,6 @@ public class TestPersonFactory {
 
     public static Person newDefaultPerson() {
         Person person = new Person();
-        person.setFirstName(DEFAULT_FIRST_NAME);
-        person.setLastName(DEFAULT_LAST_NAME);
-        return person;
-    }
-
-    public static Person newDefaultInvalidPerson() {
-        Person person = new Person();
         person.setId(DEFAULT_ID);
         person.setFirstName(DEFAULT_FIRST_NAME);
         person.setLastName(DEFAULT_LAST_NAME);
