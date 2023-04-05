9093caa7738b658fbba3b73368b3dbf7dbb09fc5
rnyPlanet <workautu@gmail.com>
2023-04-05 08:30:27 +0300

    BN-41 JavaDocs

--- src/test/java/com/bobocode/svydovets/bibernate/session/SessionTransactionTest.java
+++ src/test/java/com/bobocode/svydovets/bibernate/session/SessionTransactionTest.java
@@ -1,13 +1,15 @@
 package com.bobocode.svydovets.bibernate.session;
 
+import static com.bobocode.svydovets.bibernate.testdata.factory.TestPersonFactory.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.bobocode.svydovets.bibernate.AbstractIntegrationTest;
-import com.bobocode.svydovets.bibernate.config.BibernateConfiguration;
 import com.bobocode.svydovets.bibernate.testdata.entity.Person;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,20 +19,25 @@ class SessionTransactionTest extends AbstractIntegrationTest {
     private Session session;
 
     // todo: rework connection in tests after adding find(), save() to the session
-    @BeforeAll
-    static void initBeforeAll() {
-        sessionFactory = getSessionFactory();
-    }
+    //    @BeforeAll
+    //    static void initBeforeAll() {
+    //        sessionFactory = getSessionFactory();
+    //    }
+    //
+    //    private static SessionFactory getSessionFactory() {
+    //        BibernateConfiguration config = new BibernateConfiguration(dataSource);
+    //        config.configure();
+    //        return config.buildSessionFactory();
+    //    }
 
-    private static SessionFactory getSessionFactory() {
-        BibernateConfiguration config = new BibernateConfiguration();
-        config.configure();
-        return config.buildSessionFactory();
-    }
+    //    @BeforeEach
+    //    void beforeEach() {
+    //        session = sessionFactory.openSession();
+    //    }
 
     @BeforeEach
     void beforeEach() {
-        session = sessionFactory.openSession();
+        session = new SessionImpl(connection, searchService);
     }
 
     @Test
@@ -41,7 +48,7 @@ class SessionTransactionTest extends AbstractIntegrationTest {
 
         session.begin();
 
-        savePersonIntoDb();
+        saveDefaultPersonIntoDb();
         personsFromDb = getPersonsFromDb();
         assertEquals(3, personsFromDb.size());
 
@@ -59,7 +66,7 @@ class SessionTransactionTest extends AbstractIntegrationTest {
 
         session.begin();
 
-        savePersonIntoDb();
+        saveDefaultPersonIntoDb();
         personsFromDb = getPersonsFromDb();
         assertEquals(3, personsFromDb.size());
 
@@ -69,11 +76,24 @@ class SessionTransactionTest extends AbstractIntegrationTest {
         assertEquals(2, personsFromDb.size());
     }
 
-    private void savePersonIntoDb() {
-        session.save(new Person(3L, "test", "testovich"));
+    private void saveDefaultPersonIntoDb() throws SQLException {
+        String insertQuery = "INSERT INTO person VALUES (?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(insertQuery);
+        statement.setLong(1, DEFAULT_ID);
+        statement.setString(2, DEFAULT_FIRST_NAME);
+        statement.setString(3, DEFAULT_LAST_NAME);
+        statement.executeUpdate();
     }
 
-    private List<Person> getPersonsFromDb() {
-        return (List<Person>) session.findAll(Person.class);
+    private List<Person> getPersonsFromDb() throws SQLException {
+        ArrayList<Person> persons = new ArrayList<>();
+        ResultSet rs = connection.prepareStatement("select * from person").executeQuery();
+        while (rs.next()) {
+            var id = rs.getLong(1);
+            var firstName = rs.getString(2);
+            var secondName = rs.getString(3);
+            persons.add(new Person(id, firstName, secondName));
+        }
+        return persons;
     }
 }
