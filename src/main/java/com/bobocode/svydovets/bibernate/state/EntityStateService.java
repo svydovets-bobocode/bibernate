dcfc2e71828f45209a4a8a6cae6acad1e0dbd1fa
Vardan Matevosian <vardanmt@gmail.com>
2023-04-05 18:12:55 +0300

    Feature/BN-44/javadocs and tests for entity state service (#26)
    * BR-44 - added javadoc
    
    * BR-44 - added entity state service tests
    
    * BN-44 - changed logic for state service and added integration tests

--- src/main/java/com/bobocode/svydovets/bibernate/state/EntityStateService.java
+++ src/main/java/com/bobocode/svydovets/bibernate/state/EntityStateService.java
@@ -2,21 +2,71 @@ package com.bobocode.svydovets.bibernate.state;
 
 import com.bobocode.svydovets.bibernate.action.key.EntityKey;
 
+/** Entity state management service. This service allows to get, set and validate entity state. */
 public interface EntityStateService {
 
+    /**
+     * Allows to get current entity state by Entity key object.
+     *
+     * @param entityKey object represents unique entity {@link EntityKey}
+     * @return Entity state object {@link EntityState}
+     */
     EntityState getEntityState(EntityKey<?> entityKey);
 
+    /**
+     * Allows to get current Entity state by entity object.
+     *
+     * @param entity object represents an entity
+     * @return Entity state object {@link EntityState}
+     */
     EntityState getEntityState(Object entity);
 
-    void setEntityState(EntityKey<?> entityKey, EntityState entityState);
+    /**
+     * Allows to set entity state by entity key object. Before setting an Entity state validates if
+     * the entity key current state can be changed to entity state that passed as second argument
+     * {@link EntityState}
+     *
+     * @param entityKey object that represents a unique entity {@link EntityKey}
+     * @param toState an Entity state to be changed to.
+     */
+    void setEntityState(EntityKey<?> entityKey, EntityState toState);
 
-    void setEntityState(Object entity, EntityState entityState);
+    /**
+     * Allows to set Entity state by entity object. Before setting an entity state validates if the
+     * entity key current state can be changed to entity state that passed as second argument {@link
+     * EntityState}
+     *
+     * @param entity entity object.
+     * @param toState an Entity state to be changed to on Entity object.
+     */
+    void setEntityState(Object entity, EntityState toState);
 
-    void validate(EntityState fromState, EntityState toState);
-
+    /**
+     * Validates if an Entity state by entity key can be changed to passed state {@link EntityState}.
+     *
+     * @param entityKey object that represents a unique entity {@link EntityKey}
+     * @param toState an Entity state to be changed to on Entity object.
+     */
     void validate(EntityKey<?> entityKey, EntityState toState);
 
+    /**
+     * Validate if an Entity state by entity key is in currentState and can be changed to passed
+     * state.
+     *
+     * @param entityKey entityKey object that represents a unique entity {@link EntityKey}
+     * @param desiredState represent desired state for an entity.
+     * @param toState an Entity state to be changed to on Entity object.
+     */
+    void validate(EntityKey<?> entityKey, EntityState desiredState, EntityState toState);
+
+    /**
+     * Validates if an Entity by Entity Key can be changed to passed state {@link EntityState}.
+     *
+     * @param entity entity object
+     * @param toState an Entity state to be changed to on Entity object.
+     */
     void validate(Object entity, EntityState toState);
 
+    /** Clears all entities states. */
     void clearState();
 }
--- src/test/java/com/bobocode/svydovets/bibernate/session/SessionTransactionTest.java
+++ src/test/java/com/bobocode/svydovets/bibernate/session/SessionTransactionTest.java
@@ -1,9 +1,15 @@
 package com.bobocode.svydovets.bibernate.session;
 
+import static com.bobocode.svydovets.bibernate.state.EntityState.DETACHED;
+import static com.bobocode.svydovets.bibernate.state.EntityState.MANAGED;
+import static com.bobocode.svydovets.bibernate.state.EntityState.REMOVED;
 import static com.bobocode.svydovets.bibernate.testdata.factory.TestPersonFactory.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.bobocode.svydovets.bibernate.AbstractIntegrationTest;
+import com.bobocode.svydovets.bibernate.exception.EntityStateValidationException;
+import com.bobocode.svydovets.bibernate.state.EntityState;
 import com.bobocode.svydovets.bibernate.testdata.entity.Person;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -74,6 +80,78 @@ class SessionTransactionTest extends AbstractIntegrationTest {
 
         personsFromDb = getPersonsFromDb();
         assertEquals(2, personsFromDb.size());
+    }
+
+    @Test
+    @DisplayName("Check if the selected entity is in the MANAGED state")
+    void findAndCheckIfAnEntityIsInManagedState() throws SQLException {
+        session.begin();
+        Person personsFromDb = session.find(Person.class, 1);
+        EntityState actualEntityState = session.getEntityState(personsFromDb);
+        assertEquals(MANAGED, actualEntityState);
+        session.commit();
+    }
+
+    @Test
+    @DisplayName("Check if the session detach method set DETACHED state")
+    void detachFoundEntityAndCheckIfAnEntityIsInDetachedState() throws SQLException {
+        session.begin();
+        Person personsFromDb = session.find(Person.class, 1);
+        session.detach(personsFromDb);
+        EntityState actualEntityState = session.getEntityState(personsFromDb);
+        assertEquals(DETACHED, actualEntityState);
+        session.commit();
+    }
+
+    @Test
+    @DisplayName("Check if the entity is in MANAGED state after merge method call")
+    void mergeDetachedEntityAndCheckIfAnEntityIsInManagedState() throws SQLException {
+        session.begin();
+        Person personsFromDb = session.find(Person.class, 1);
+        session.detach(personsFromDb);
+        session.merge(personsFromDb);
+        EntityState actualEntityState = session.getEntityState(personsFromDb);
+        assertEquals(MANAGED, actualEntityState);
+        session.commit();
+    }
+
+    @Test
+    @DisplayName("Check if the entity is in REMOVED state after delete method call")
+    void removeEntityAndCheckIfAnEntityIsInRemovedState() throws SQLException {
+        session.begin();
+        Person personsFromDb = session.find(Person.class, 1);
+        session.delete(personsFromDb);
+        EntityState actualEntityState = session.getEntityState(personsFromDb);
+        assertEquals(REMOVED, actualEntityState);
+        session.commit();
+    }
+
+    @Test
+    @DisplayName("Should throw en exception when merge entity in TRANSIENT state")
+    void shouldThrowAnExceptionWhenMergeEntityThatIsInTransientState() throws SQLException {
+        session.begin();
+
+        Person personsFromDb = new Person(1L, "Transient person", "Last name");
+
+        assertThrows(
+                EntityStateValidationException.class,
+                () -> session.merge(personsFromDb),
+                "Entity state should be in DETACHED, but was in TRANSIENT");
+        session.commit();
+    }
+
+    @Test
+    @DisplayName("Should throw en exception when delete entity in TRANSIENT state")
+    void shouldThrowAnExceptionWhenDeleteEntityThatIsInTransientState() throws SQLException {
+        session.begin();
+
+        Person personsFromDb = new Person(1L, "Transient person", "Last name");
+
+        assertThrows(
+                EntityStateValidationException.class,
+                () -> session.delete(personsFromDb),
+                "an't change entity state from TRANSIENT to REMOVED");
+        session.commit();
     }
 
     private void saveDefaultPersonIntoDb() throws SQLException {
