81013a6d56418a062e0caf7868e25439492eb2a4
Stanislav <33624178+rnyPlanet@users.noreply.github.com>
2023-04-05 18:15:58 +0300

    BN-41 Session & Transaction JavaDocs (#28)
    * BN-41 JavaDocs

--- src/main/java/com/bobocode/svydovets/bibernate/transaction/Transaction.java
+++ src/main/java/com/bobocode/svydovets/bibernate/transaction/Transaction.java
@@ -1,9 +1,33 @@
 package com.bobocode.svydovets.bibernate.transaction;
 
+import com.bobocode.svydovets.bibernate.exception.TransactionException;
+import com.bobocode.svydovets.bibernate.session.Session;
+
+/**
+ * Interface used to control transaction per {@link Session}. The {@link Session} uses the
+ * associated {@link Transaction} object.
+ */
 public interface Transaction {
+    /**
+     * Start a transaction.
+     *
+     * @throws TransactionException if {@code isStarted} is true
+     */
     void begin();
 
+    /**
+     * Commit the current transaction, writing any unflushed changes to the database.
+     *
+     * @throws TransactionException if {@code isStarted} is false
+     * @throws TransactionException if the commit fails
+     */
     void commit();
 
+    /**
+     * Roll back the current transaction.
+     *
+     * @throws TransactionException if {@code isStarted} is false
+     * @throws TransactionException if an unexpected error condition is encountered
+     */
     void rollback();
 }
