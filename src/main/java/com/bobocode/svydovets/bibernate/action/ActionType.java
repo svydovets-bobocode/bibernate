bf3dc4278e4e78849e92592a1db807fbcfb0c0b7
sleepnotdead <37627067+sleepnotdead@users.noreply.github.com>
2023-04-06 18:24:05 +0300

    BN-46: Add tests. (#34)
    BN-46: Add tests for ActionQueue.
    
    Co-authored-by: Mikhail Borisovskiy <borysovm@amdocs.com>
    Co-authored-by: Nicolae <66087568+Prosperro-de@users.noreply.github.com>

--- src/main/java/com/bobocode/svydovets/bibernate/action/ActionType.java
+++ src/main/java/com/bobocode/svydovets/bibernate/action/ActionType.java
@@ -1,7 +1,17 @@
 package com.bobocode.svydovets.bibernate.action;
 
 public enum ActionType {
-    INSERT,
-    UPDATE,
-    REMOVE
+    INSERT(1),
+    UPDATE(2),
+    REMOVE(3);
+
+    private final int priority;
+
+    ActionType(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
--- src/main/java/com/bobocode/svydovets/bibernate/session/SessionImpl.java
+++ src/main/java/com/bobocode/svydovets/bibernate/session/SessionImpl.java
@@ -183,7 +183,7 @@ public class SessionImpl implements Session {
     public void flush() {
         verifySessionIsOpened();
         performDirtyChecking();
-        actionQueue.executeAll();
+        actionQueue.executeAllWithOrder();
         actionQueue.clear();
     }
 
