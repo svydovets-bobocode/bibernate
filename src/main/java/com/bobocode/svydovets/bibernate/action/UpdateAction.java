65dc1fcc4288c9084ecad14fe38b8fdbb90481c1
Stanislav <33624178+rnyPlanet@users.noreply.github.com>
2023-04-07 11:01:12 +0300

    BN- Javadocs (#38)
    * BN- Javadocs

--- src/main/java/com/bobocode/svydovets/bibernate/action/UpdateAction.java
+++ src/main/java/com/bobocode/svydovets/bibernate/action/UpdateAction.java
@@ -8,12 +8,27 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Optional;
 
+/**
+ * A class that represents an update action to be performed on a database entity. The action updates
+ * the fields of the entity with the values in the provided actionObject according to the
+ * corresponding database table row that has the same ID as the entity.
+ */
 public class UpdateAction<T> extends AbstractAction<T> {
 
     public UpdateAction(Connection connection, T actionObject) {
         super(connection, actionObject);
     }
 
+    /**
+     * Executes the update action on the database by updating the corresponding row of the provided
+     * entity object. The method creates an SQL update query using the SqlQueryBuilder class and sets
+     * the values of the entity's updatable fields to the corresponding placeholders in the query. If
+     * the entity has a version field, the query is modified to include the version field in the where
+     * condition. The method then executes the prepared statement and throws an exception if the
+     * number of updated rows is not exactly one.
+     *
+     * @throws BibernateException if there is an error executing the update query
+     */
     @Override
     protected void doExecute() {
         var actionObjectType = actionObject.getClass();
--- src/main/java/com/bobocode/svydovets/bibernate/action/key/EntityKey.java
+++ src/main/java/com/bobocode/svydovets/bibernate/action/key/EntityKey.java
@@ -1,13 +1,28 @@
 package com.bobocode.svydovets.bibernate.action.key;
 
+import com.bobocode.svydovets.bibernate.session.Session;
+import com.bobocode.svydovets.bibernate.session.SessionImpl;
 import com.bobocode.svydovets.bibernate.util.EntityUtils;
 
+/**
+ * Entity key is used to determine a uniqueness of entities in {@link Session}. Used as a key in
+ * {@link SessionImpl#entitiesSnapshotMap} and {@link SessionImpl#entitiesCacheMap}
+ *
+ * @param type entity class
+ * @param id primary key
+ */
 public record EntityKey<T>(Class<T> type, Object id) {
 
     public static <T> EntityKey<T> of(Class<T> type, Object id) {
         return new EntityKey<>(type, id);
     }
 
+    /**
+     * Returns an EntityKey instance for the specified entity object.
+     *
+     * @param entity the entity object for which to retrieve an EntityKey
+     * @return an EntityKey instance for the specified entity object
+     */
     public static <T> EntityKey<?> valueOf(T entity) {
         var id = EntityUtils.retrieveIdValue(entity);
         return new EntityKey<>(entity.getClass(), id.orElse(null));
