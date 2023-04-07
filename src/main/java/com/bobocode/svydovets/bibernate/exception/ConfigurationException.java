9ced46d5e19a70aaa7bd55d6acf60ae1da74b410
rnyPlanet <workautu@gmail.com>
2023-04-07 19:11:09 +0300

    BN- Сheck if config file exists

--- src/main/java/com/bobocode/svydovets/bibernate/exception/ConfigurationException.java
+++ src/main/java/com/bobocode/svydovets/bibernate/exception/ConfigurationException.java
@@ -5,6 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ConfigurationException extends BibernateException {
 
+    public ConfigurationException(String message) {
+        super(message);
+    }
+
     public ConfigurationException(String message, Throwable cause) {
         super(message, cause);
     }
